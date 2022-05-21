ae8cdb17112cb2df5913350fc423f71bb769deb3
Carissa Farry <carissafarry@gmail.com>
2022-05-21 12:42:14 +0700

    delete unused

--- includes/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-function
-$kamus = new Kamus();
-$kamus->tambah(‘big’, [‘large’, ‘great’]);
-$kamus->tambah(‘big’, [‘huge’, ‘fat’]);
-$kamus->tambah(‘huge’, [‘enormous’, ‘gigantic’]);
-
-$kamus->ambilSinonim(‘big’);
